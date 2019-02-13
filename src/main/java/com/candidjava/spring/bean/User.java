80a26ef8545221406e75064f0077c9611acb3d2f
anathakur <anathakur@USBLRANATHAKUR1.us.deloitte.com>
2019-02-13 17:25:14 +0530

    This is created in Eclipse and now being pushed to GIT

--- src/main/java/com/candidjava/spring/bean/User.java
+++ src/main/java/com/candidjava/spring/bean/User.java
@@ -49,4 +49,14 @@ public class User {
 		this.country = country;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
 }
